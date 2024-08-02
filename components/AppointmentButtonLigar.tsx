3169d59d30061b5ba12d648632210d55301eff71
Danielle_Zeff <77025401+DanielleZeff@users.noreply.github.com>
2024-08-02 16:28:52 -0300

    Address and Button Call to Specialist

---
+++ components/AppointmentButtonLigar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Link from "next/link";
+import { Button, styled } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import { dosis } from "@/fonts/Dosis";
+
+const BootstrapButton = styled(Button)({
+  "&:hover": {
+    backgroundColor: "#78acb1",
+    color: "#fff",
+  },
+});
+
+const AppointmentButtonLigar = () => {
+  return (
+    <>
+      <Link href="#appointment">
+        <BootstrapButton
+          className={dosis.className}
+          sx={{
+            backgroundColor: "#038C8C",
+            borderRadius: "28px",
+            color: "#fff",
+            fontWeight: 700,
+            mt: 3,
+            ml: 4,
+            width: "200px",
+            height: "48px",
+          }}
+        >
+          <PhoneIphoneIcon sx={{ color: "#add9d4" }} />
+          &nbsp; Ligar ao especialista
+        </BootstrapButton>
+      </Link>
+    </>
+  );
+};
+
+export default AppointmentButtonLigar;
