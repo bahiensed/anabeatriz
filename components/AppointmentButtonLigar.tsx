85d8ea84151f47e516dbbca1b6b62d4070374d67
Danielle_Zeff <77025401+DanielleZeff@users.noreply.github.com>
2024-08-06 13:25:29 -0300

    AppointmentButtonLigar is working

--- components/AppointmentButtonLigar.tsx
+++ components/AppointmentButtonLigar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import { Button, styled } from "@mui/material";
+import { Button, Stack, styled, Typography } from "@mui/material";
 import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 import { dosis } from "@/fonts/Dosis";
+import { useState } from "react";
+import { openSans } from "@/fonts/OpenSans";
 
 const BootstrapButton = styled(Button)({
   "&:hover": {
@@ -13,26 +15,76 @@ const BootstrapButton = styled(Button)({
 });
 
 const AppointmentButtonLigar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
   return (
     <>
-      <Link href="#appointment">
-        <BootstrapButton
-          className={dosis.className}
-          sx={{
-            backgroundColor: "#038C8C",
-            borderRadius: "28px",
-            color: "#fff",
-            fontWeight: 700,
-            mt: 3,
-            ml: 4,
-            width: "200px",
-            height: "48px",
-          }}
-        >
-          <PhoneIphoneIcon sx={{ color: "#add9d4" }} />
-          &nbsp; Ligar ao especialista
-        </BootstrapButton>
-      </Link>
+      <BootstrapButton
+        onClick={handleClick}
+        className={dosis.className}
+        sx={{
+          backgroundColor: "#038C8C",
+          borderRadius: "28px",
+          color: "#fff",
+          fontWeight: 700,
+          mt: 3,
+          ml: 4,
+          width: "200px",
+          height: "48px",
+        }}
+      >
+        <PhoneIphoneIcon sx={{ color: "#add9d4" }} />
+        &nbsp; Ligar ao especialista
+      </BootstrapButton>
+      {isClicked ? (
+        <>
+          <Typography
+            className={dosis.className}
+            sx={{
+              color: "#333",
+              fontSize: "18px",
+              fontWeight: "600",
+              mt: 5,
+              ml: 5,
+              letterSpacing: "0.05em",
+              lineHeight: 1.1,
+            }}
+          >
+            AGENDAR CONSULTA:
+          </Typography>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            sx={{
+              mt: 1,
+            }}
+          >
+            <PhoneIphoneIcon sx={{ color: "#add9d4", mt: 1, ml: 5 }} />
+            <Stack direction="column" justifyContent="center">
+              <Typography
+                className={openSans.className}
+                sx={{
+                  color: "#333",
+                  fontSize: "18px",
+                  fontWeight: "300",
+                  mt: 0.8,
+
+                  letterSpacing: "0.05em",
+                  lineHeight: 1.5,
+                }}
+              >
+                +55 00 00000-000
+              </Typography>
+            </Stack>
+          </Stack>
+        </>
+      ) : (
+      null
+      )}
     </>
   );
 };
--- components/OnlineAppointments.tsx
+++ components/OnlineAppointments.tsx
@@ -1,9 +1,17 @@
+"use client"
+
+import { useState } from "react";
 import { Stack, Typography } from "@mui/material";
+import AppointmentButtonLigar from "@/components/AppointmentButtonLigar";
 import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 import { dosis } from "@/fonts/Dosis";
 import { openSans } from "@/fonts/OpenSans";
 
+
 const OnlineAppointments = () => {
+
+
+
   return (
     <Stack
       sx={{
@@ -11,7 +19,7 @@ const OnlineAppointments = () => {
         mt: "1150px",
         position: "absolute",
         width: "100%",
-        height: "278px",
+        height: "350px",
       }}
     >
       <Stack
@@ -31,43 +39,8 @@ const OnlineAppointments = () => {
         >
           Telemedicina (Consultas Online)
         </Typography>
-        <Typography
-          className={dosis.className}
-          sx={{
-            color: "#333",
-            fontSize: "18px",
-            fontWeight: "600",
-            mt: 5,
-            letterSpacing: "0.05em",
-            lineHeight: 1.1,
-          }}
-        >
-          AGENDAR CONSULTA:
-        </Typography>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          sx={{
-            mt: 1,
-          }}
-        >
-          <PhoneIphoneIcon sx={{ color: "#add9d4", mt: 1 }} />
-          <Stack direction="column" justifyContent="center">
-            <Typography
-              className={openSans.className}
-              sx={{
-                color: "#333",
-                fontSize: "18px",
-                fontWeight: "300",
-                mt: 0.8,
-                letterSpacing: "0.05em",
-                lineHeight: 1.5,
-              }}
-            >
-              +55 00 00000-000
-            </Typography>
-          </Stack>
-        </Stack>
+        <AppointmentButtonLigar/>
+      
       </Stack>
     </Stack>
   );
