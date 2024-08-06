87e50e77fb73538f5d238e834329e62f2a1f7582
Danielle_Zeff <77025401+DanielleZeff@users.noreply.github.com>
2024-08-06 14:59:16 -0300

    Price

--- components/Price.tsx
+++ components/Price.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import * as React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { dosis } from "@/fonts/Dosis";
+import { openSans } from "@/fonts/OpenSans";
 
-const Price = () => {
-  return <div>Price</div>;
-};
+function createData(name: string, price: string) {
+  return { name, price };
+}
 
-export default Price;
+const rows = [
+  createData("Consulta", " Desde 700 BRL "),
+  createData("Consulta", " Desde 700 BRL "),
+  createData("Consulta", " Desde 700 BRL "),
+  createData("Consulta", " Desde 700 BRL "),
+  createData("Consulta", " Desde 700 BRL "),
+  createData("Consulta", " Desde 700 BRL "),
+];
+
+export default function Price() {
+  return (
+    <>
+      <Typography
+        className={dosis.className}
+        sx={{
+          color: "#333",
+          fontSize: "36px",
+          fontWeight: 500,
+          lineHeight: 1.1,
+          textAlign: "center",
+        }}
+      >
+        PREÇO
+      </Typography>
+      <TableContainer
+        component={Paper}
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+          m: "0 auto",
+          mb: 10,
+          mt: 5,
+          width: "700px",
+        }}
+      >
+        <Table aria-label="simple table">
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell
+                  component="th"
+                  scope="row"
+                  className={openSans.className}
+                  sx={{
+                    color: "#333",
+                    fontSize: "16px",
+                    fontWeight: 600,
+                    lineHeight: 1.5,
+                  }}
+                >
+                  {row.name}
+                </TableCell>
+                <TableCell
+                align="right"
+                  className={openSans.className}
+                  sx={{
+                    color: "#43aae6",
+                    fontSize: "16px",
+                    fontWeight: 600,
+                    lineHeight: 1.5,
+                  }}
+                >
+                  {row.price}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
