244cee4949d3935ecdde7e85efa86e6238dd6d8c
Danielle_Zeff <77025401+DanielleZeff@users.noreply.github.com>
2024-08-12 15:41:09 -0300

    Arrow Down

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,14 +2,20 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { AppBar, Stack, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material";
 import Navigation from "@/components/Navigation";
 import NavBarButtonVisita from "@/components/NavBarButtonVisita";
+import ArrowCircleDown from "@mui/icons-material/ArrowCircleDown";
 import { dosis } from "@/fonts/Dosis";
 import clinic from "@/public/images/clinic.png";
 
 function NavBar() {
   const [stickyClass, setStickyClass] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsVisible(!isVisible);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", stickNavbar);
@@ -25,44 +31,60 @@ function NavBar() {
   };
 
   return (
-    <AppBar
-      className={`navbar ${stickyClass}`}
-      sx={{
-        backgroundColor: "#fff",
-        
-      }}
-      position="sticky"
-    >
-      <Toolbar id="aboutme" disableGutters>
-        <Stack direction="row" justifyContent="center" spacing={2}>
-          <Image
-            src={clinic}
-            alt="clinic"
-            style={{ width: "78px", height: "80px" }}
-          />
-          <Stack direction="column" justifyContent="center">
-            <Typography
-              className={dosis.className}
-              variant="h6"
-              sx={{ color: "#000", lineHeight: 1 }}
-            >
-              ANA BEATRIZ WINTER
-            </Typography>
-            <Typography
-              className={dosis.className}
-              sx={{ color: "gray", fontSize: "12px" }}
-            >
-              ENDOCRINOLOGIA & METABOLOGIA
-            </Typography>
-          </Stack>
-        </Stack>
-        <Navigation />
+    <>
+      <Stack alignItems="center" justifyContent="center">
+        <ArrowCircleDown
+          onClick={toggleMenu}
+          sx={{
+            background: "none",
+            backgroundColor: "none",
+            color: "#cab882",
+            cursor: "pointer",
+            width: "48px",
+            height: "48px",
+          }}
+        />
+      </Stack>
+      {isVisible && (
+        <AppBar
+          className={`navbar ${stickyClass}`}
+          sx={{
+            backgroundColor: "#fff",
+          }}
+          position="sticky"
+        >
+          <Toolbar id="aboutme" disableGutters>
+            <Stack direction="row" justifyContent="center" spacing={2}>
+              <Image
+                src={clinic}
+                alt="clinic"
+                style={{ width: "78px", height: "80px" }}
+              />
+              <Stack direction="column" justifyContent="center">
+                <Typography
+                  className={dosis.className}
+                  variant="h6"
+                  sx={{ color: "#000", lineHeight: 1 }}
+                >
+                  ANA BEATRIZ WINTER
+                </Typography>
+                <Typography
+                  className={dosis.className}
+                  sx={{ color: "gray", fontSize: "12px" }}
+                >
+                  ENDOCRINOLOGIA & METABOLOGIA
+                </Typography>
+              </Stack>
+            </Stack>
+            <Navigation />
 
-        <Stack direction="column">
-          <NavBarButtonVisita />
-        </Stack>
-      </Toolbar>
-    </AppBar>
+            <Stack direction="column">
+              <NavBarButtonVisita />
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      )}
+    </>
   );
 }
 export default NavBar;
