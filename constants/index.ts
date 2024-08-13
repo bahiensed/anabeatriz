c1419450e8ac93e73b68288df51fdc27570a883d
dbahiense <douglas@rohling.com.br>
2024-08-13 15:21:09 -0300

    all constants in only one file

---
+++ constants/index.ts
@@ -0,0 +1,135 @@
+export const cardsData = [
+  {
+    title: "Experiências Profissionais",
+    icon: "Chat",
+    content: [
+        "Professora associada de Endocrinologia na faculdade de Ciências Médicas (FCM) da UERJ desde 2016.",
+        "Secretaria executiva da sociedade brasileira de endocrinooogia -regional RJ (2020/22 e 2023/24).",
+        "Coordenadora de pesquisa da Faculdade de Ciências Médicas da FCM (2024/28).",
+        "Professora do programa de pós-graduação em Fisiopatolologia Clínica e Experimental da FCM UERJ.",
+        "Ex-professora da Faculdade de Medicina de Petrópolis (2007-2023).",
+        "Ex-chefe do Departamento de Medicina Interna da FCM UERJ (2021/23).",
+        "Médica endocrinologista do Hospital Umiversitario Pedro Ernesto da UERJ (2012-2016).",
+    ]
+  },
+  {
+    title: "Formação Acadêmica",
+    icon: "School",
+    content: [
+      "Doutorado em Endocrinologia, UFRJ, Rio de Janeiro, 2015",      
+      "Mestrado em endocrinologia, UFRJ, Rio de Janeiro, 2009",
+      "Especialista em Endocrinologia e Metodologia, SBEM, Rio de Janeiro, 2007 ",
+      "Residência em Endocrinologia e Metabologia, IEDE, Rio de Janeiro, 2007",
+      "Residência em Clínica Médica, UFJF, Juiz de Fora, 2005",
+      "Graduação em Medicina, FASE, Petrópolis, 2002",
+    ]
+  },
+  {
+    title: "Registros Profissionais",
+    icon: "HowToReg",
+    content: [
+      "CRM RJ 737470",
+      "RQE Endocrinologia e Metabologia 14091",
+      "RQE Clínica Médica 15964",      
+    ]
+  },
+  {
+    title: "Tratamentos",
+    icon: "MonitorHeart",
+    content: [
+      "Lorem ipsum eget risus tristique odio lacus volutpat, praesent ipsum torquent amet condimentum pellentesque aptent, rutrum iaculis nulla arcu fames luctus.",
+    ]
+  },
+  {
+    title: "Prêmios",
+    icon: "EmojiEvents",
+    content: [
+      "Lorem ipsum eget risus tristique odio lacus volutpat, praesent ipsum torquent amet condimentum pellentesque aptent, rutrum iaculis nulla arcu fames luctus. ",
+    ]
+  },
+  {
+    title: "Publicações",
+    icon: "Article",
+    content: [
+      "Lorem ipsum eget risus tristique odio lacus volutpat, praesent ipsum torquent amet condimentum pellentesque aptent, rutrum iaculis nulla arcu fames luctus.leo lectus venenat consectetur dui ad sagittis sapien integer nullam, eu habitant leo potenti id interdum dolor neque ante, placerat metus bibendum commodo suscipit massa nisl posuere. neque class auctor facilisis aenean est lobortis molestie posuere, amet loborti habitant quam leo dictum quam congue, vestibulum est dictumst convallis pellentesque felis lacinia. laoreet est pharetra varius justo suspendisse lorem",
+    ]
+  },
+  {
+    title: "Redes Sociais",
+    icon: "FilterDrama",
+    content: [
+      "Lorem ipsum eget risus tristique odio lacus volutpat, praesent ipsum torquent amet condimentum pellentesque aptent, rutrum iaculis nulla arcu fames luctus."
+    ]
+  },
+];
+
+export const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
+    title: "Snacks",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
+    title: "Tower",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
+    title: "Tree",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
+    title: "Camping Car",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
+    title: "Mountain",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
