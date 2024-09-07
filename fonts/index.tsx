2e8ed6056a59be261f741e5a5baeee2de23e7421
dbahiense <douglas@rohling.com.br>
2024-09-07 10:30:50 -0300

    feat: add Inter font alongside Dosis and Open Sans

--- fonts/index.tsx
+++ fonts/index.tsx
@@ -1,6 +1,7 @@
 import { Dosis } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Open_Sans } from "next/font/google";
 
 export const dosis = Dosis({ subsets: ["latin"] });
-
+export const inter = Inter({ subsets: ["latin"] });
 export const openSans = Open_Sans({ subsets: ["latin"] });
