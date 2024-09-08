d96106a7d1555d07645c1a47bd8af57a6a94c705
dbahiense <douglas@rohling.com.br>
2024-09-08 13:35:24 -0300

    feat: about cards

---
+++ components/About/index.tsx
@@ -0,0 +1,42 @@
+import { Box, Typography } from "@mui/material";
+import AboutCards from "@/components/About/AboutCards";
+import { dosis } from "@/fonts";
+
+const About = () => {
+  return (
+    <>
+      <Box
+      id="about"
+      sx={{
+        alignItems: "center",
+        color: "#343434",
+        justifyContent: "center",
+        pl: {xs: 3, md: 12},
+        pr: {xs: 0, md: 12},
+        py: 6,
+      }}
+    >
+      <Typography
+        className={dosis.className}
+        sx={{
+          color: "#343434",
+          fontSize: { xs: '32px', md: '42px' },
+          fontWeight: 500,
+          letterSpacing: 2,
+          mr: 2,
+          pb: 8,
+          textAlign: 'center',
+          textTransform: 'uppercase'
+        }}
+      >
+        Informações sobre mim
+      </Typography>
+
+      <AboutCards />
+
+      </Box>
+    </>
+  );
+};
+
+export default About;
