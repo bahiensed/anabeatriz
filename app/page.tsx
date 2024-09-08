0946a471370baab4a642df644b9ab6543a151673
dbahiense <douglas@rohling.com.br>
2024-09-08 13:30:29 -0300

    refactor: organize components structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
-import AboutMe from "@/components/AboutMe";
-import Appointment from "@/components/Appointment";
-import BannerAppointment from "@/components/BannerAppointment";
-import Form from "@/components/Form";
-import Galery from "@/components/Galery";
+import About from "@/components/About";
+import Banner from "@/components/Banner";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer/Footer";
+import Gallery from "@/components/ImageGallery";
+import Map from "@/components/Map";
 import NavBar from "@/components/NavBar";
-import SocialProofCarousel from "../components/SocialProofCarousel";
-import { Stack } from "@mui/material";
+import Review from "@/components/Reviews";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <Stack>
-      <Stack sx={{mt:"100vh"}}>
-      <NavBar />
-      </Stack>
-      
-      <AboutMe />
-      <Appointment />
-      <SocialProofCarousel />
-      <Galery />
-      <Form />
-    </Stack>
+    <>
+      <Banner />
+
+      <Box sx={{ mt:"100vh" }}>
+        <NavBar />
+      </Box>
+            
+      <About />
+      <Map />
+      <Review />
+      <Gallery />
+      <Contact />
+      <Footer />
+    </>
   );
 }
