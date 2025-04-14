3c480b2033e0ce453a90bdba1733562d29e88b8c
Douglas Bahiense <douglas@rohling.com.br>
2025-04-14 10:31:09 -0300

    chore: rename file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import About from "@/components/About";
-import Banner from "@/components/Banner";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer/Footer";
-import Gallery from "@/components/ImageGallery";
-import Map from "@/components/Map";
-import NavBar from "@/components/NavBar";
-import Review from "@/components/Reviews";
-import { Box } from "@mui/material";
+import About from '@/components/About'
+import Banner from '@/components/Banner'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+import Gallery from '@/components/ImageGallery'
+import Map from   '@/components/Map'
+import NavBar from '@/components/NavBar'
+import Review from '@/components/Reviews'
+import { Box } from '@mui/material'
 
 export default function Home() {
   return (
